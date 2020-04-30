f944dac647b5a2006d4874d42a199a7cb206dfc4
josepheschen <jeschen@rams.colostate.edu>
2020-04-30 13:47:24 -0600

    imports fixed, calculation of winning added, payouts added

--- contracts/Roullete.sol
+++ contracts/Roullete.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.16;
-import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+import "github.com/oraclize/ethereum-api/provableAPI.sol";
 
-contract RinkebyRoullete is usingOraclize {
+contract RinkebyRoullete is usingProvable {
 
     mapping (address => uint256) public accountBalance;
     address payable rouletteOwner;
@@ -11,10 +11,13 @@ contract RinkebyRoullete is usingOraclize {
     //acceptable specifics
     uint[] acceptableBetSpecifics;
 
+    bool public betHasBeenMade;
+
     constructor() public {
         rouletteOwner = msg.sender;
         winningsMultipliers = [35, 11, 5, 1, 1];
         acceptableBetSpecifics = [36, 11 , 2, 1, 1];
+        betHasBeenMade = false;
     }
 
     /* We are going to be handling 5 different kinds of bets:
@@ -42,9 +45,19 @@ contract RinkebyRoullete is usingOraclize {
     }
 
     Bet currentBet;
+<<<<<<< Updated upstream
     bool public betHasBeenMade = false;
+=======
+
+    // Leaving this public for testing. Will want to make non public after we are sure that random number generation works
+    uint256 public randomNumber;
+
+    event LogNewProvableQuery(string description);
+    event LogNewRandomNumber(string number);
+>>>>>>> Stashed changes
 
     function placeBet(uint8 _bType, uint64 _bSpecifics) payable public  {
+        require(betHasBeenMade == false);
         //first make sure there was no tampering with how much was paid and the call of the funtion tracking the amount
         //require(msg.value / 1000000000000000000 /* this is the eth to wei conversion, now our units are in ETH*/  == _bAmount);
 
@@ -101,10 +114,56 @@ contract RinkebyRoullete is usingOraclize {
         update();
         //check to see if user has won
 
+        bool didUserWin = didWin();
 
-        //if they have, payout
-        //if they didnt, put their account balance to 0, we already have the money because its part of the contract now
+        if (didUserWin) {
+            uint256 winnings = currentBet.betAmount * winningsMultipliers[currentBet.betType];
+            accountBalance[currentBet.player] = accountBalance[currentBet.player] + winnings - currentBet.betAmount;
+            //subtracting original bet amount b/c it's added at the beginning of the round
+        } else {
+            accountBalance[currentBet.player] = accountBalance[currentBet.player] - currentBet.betAmount;
+        }
+    }
 
+    function didWin() private view returns (bool) {
+        if (currentBet.betType == 0) {
+            if (randomNumber == currentBet.betSpecifics) { // striaght up
+                return true;
+            }
+        } else if (currentBet.betType == 1) { // street or row
+
+            if(randomNumber == 0) return false;
+            if((randomNumber - 1) / 3 == currentBet.betSpecifics) return true;
+
+        } else if (currentBet.betType == 2) { // column
+            if (randomNumber % 3 == 1) {
+                if (currentBet.betSpecifics == 0) return true;
+            }
+            if (randomNumber % 3 == 2) {
+                if (currentBet.betSpecifics == 1) return true;
+            }
+            if (randomNumber % 3 == 0) {
+                return (currentBet.betSpecifics == 2);
+            }
+        } else if (currentBet.betType == 3) { // color
+            if (currentBet.betSpecifics == 0) { // black
+                if (randomNumber <= 10 || (randomNumber >= 20 && randomNumber <= 28)) {
+                    return (randomNumber % 2 == 0);
+                } else {
+                    return (randomNumber % 2 == 1);
+                }
+            } else { // red
+                if (randomNumber <= 10 || (randomNumber >= 20 && randomNumber <= 28)) {
+                    return (randomNumber % 2 == 1);
+                } else {
+                    return (randomNumber % 2 == 0);
+                }
+            }
+        } else if (currentBet.betType == 4) { // odd/even
+            if( (currentBet.betSpecifics == 0) && (randomNumber % 2 == 0)) return true;
+            else if ((currentBet.betSpecifics == 1) && (randomNumber % 2 == 1)) return true;
+        }
+        return false;
     }
 
     uint256 public randomNumber;
